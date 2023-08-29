cd3942babc3a40b5aade2b3b308884b3968611e6
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-29 12:08:30 +0200

    deleted unnecessary styles

--- src/StepsLayout/styled.ts
+++ src/StepsLayout/styled.ts
@@ -4,7 +4,8 @@ export const Container = styled.div`
   padding: 30px 60px;
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 90%;
+  min-width: 600px;
 `;
 
 export const Title = styled.h2`
@@ -16,39 +17,4 @@ export const Title = styled.h2`
 export const Description = styled.p`
   margin: 10px 0;
   color: ${({ theme }) => theme.colors.coolGray};
-`;
-
-export const ChangedContent = styled.div`
-  flex: 1;
-`;
-
-export const ButtonsBox = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-export const Button = styled.button`
-  padding: 16px 20px;
-  font-weight: 500;
-  font-size: 18px;
-  color: ${({ theme }) => theme.colors.alabaster};
-  background-color: ${({ theme }) => theme.colors.marineBlue};
-  border-radius: 10px;
-  border: none;
-  cursor: pointer;
-  transition: all 0.5s ease-in-out;
-
-  &:hover {
-    filter: brightness(130%);
-  }
-`;
-
-export const BackButton = styled(Button)`
-  background-color: transparent;
-  color: ${({ theme }) => theme.colors.coolGray};
-
-  &:hover {
-    filter: brightness(0);
-    color: ${({ theme }) => theme.colors.marineBlue};
-  }
 `;
