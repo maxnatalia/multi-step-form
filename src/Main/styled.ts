13f624dafa79ee13b5a149c0d4ec1c7a42352837
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-29 12:07:22 +0200

    improved height for Main in styles

--- src/Main/styled.ts
+++ src/Main/styled.ts
@@ -5,7 +5,6 @@ export const StyledMain = styled.main`
   border-radius: 10px;
   box-shadow: 2px 2px 23px ${({ theme }) => theme.colors.lightGray};
   background-color: ${({ theme }) => theme.colors.alabaster};
-  max-width: 975px;
   height: 600px;
   display: flex;
 `;
