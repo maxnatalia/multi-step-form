f72c2730e0d96e81032cec0751f57b405191ebb3
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-29 12:08:16 +0200

    deleted ButtonsBox and unnecessary styles

--- src/StepsLayout/index.tsx
+++ src/StepsLayout/index.tsx
@@ -1,14 +1,5 @@
 import { ReactNode } from "react";
-import {
-  BackButton,
-  Button,
-  ButtonsBox,
-  ChangedContent,
-  Container,
-  Description,
-  Title,
-} from "./styled";
-
+import { Container, Description, Title } from "./styled";
 interface StepsLayoutProps {
   title: string;
   description: string;
@@ -20,11 +11,7 @@ const StepsLayout = ({ title, description, children }: StepsLayoutProps) => {
     <Container>
       <Title>{title}</Title>
       <Description>{description}</Description>
-      <ChangedContent>{children}</ChangedContent>
-      <ButtonsBox>
-        <BackButton>Go Back</BackButton>
-        <Button>Next Step</Button>
-      </ButtonsBox>
+      <div>{children}</div>
     </Container>
   );
 };
