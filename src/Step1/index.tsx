0d64663c3e54f9774c0c98537ec42d5409f143c5
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-31 12:04:46 +0200

    added handling errors

--- src/Step1/index.tsx
+++ src/Step1/index.tsx
@@ -1,9 +1,17 @@
 import StepsLayout from "../StepsLayout";
-import { StepsProps } from "../useMultiStepForm";
+import { ErrorProps, StepsProps } from "../useMultiStepForm";
 import Label from "./Label";
 import { Box, Input } from "./styled";
+interface Step1Props extends StepsProps {
+  error: ErrorProps;
+}
 
-const Step1 = ({ field, handleFieldChange }: StepsProps) => {
+const Step1 = ({
+  field,
+  handleFieldChange,
+  error,
+  userInteracted,
+}: Step1Props) => {
   if (!field) {
     field = {
       name: "",
@@ -18,24 +26,42 @@ const Step1 = ({ field, handleFieldChange }: StepsProps) => {
       description="Please provide your name, email address, and phone number."
     >
       <Box>
-        <Label fieldName="Name" />
+        <Label
+          fieldName="Name"
+          error={error}
+          inputName="name"
+          userInteracted={userInteracted}
+        />
         <Input
+          error={error.name ? "true" : "false"}
           type="text"
           placeholder="e.g. Stephen King"
           name="name"
           value={field.name}
           onChange={handleFieldChange}
         />
-        <Label fieldName="Email Address" />
+        <Label
+          fieldName="Email Address"
+          error={error}
+          inputName="email"
+          userInteracted={userInteracted}
+        />
         <Input
+          error={error.email ? "true" : "false"}
           type="email"
           placeholder="e.g. stephenking@lorem.com"
           name="email"
           value={field.email}
           onChange={handleFieldChange}
         />
-        <Label fieldName="Phone Number" />
+        <Label
+          fieldName="Phone Number"
+          error={error}
+          inputName="phone"
+          userInteracted={userInteracted}
+        />
         <Input
+          error={error.phone ? "true" : "false"}
           type="tel"
           placeholder="e.g. +1 234 567 890"
           name="phone"
