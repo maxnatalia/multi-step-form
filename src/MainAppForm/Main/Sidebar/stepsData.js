372abec97cbff87a3bafadf5b30ec118a82ba89e
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-03 08:47:25 +0200

    changed paths for Sidebar

---
+++ src/MainAppForm/Main/Sidebar/stepsData.js
@@ -0,0 +1,22 @@
+export const stepsData = [
+    {
+        number: "1",
+        content: "Step 1",
+        name: "Your Info"
+    },
+    {
+        number: "2",
+        content: "Step 2",
+        name: "Select Plan"
+    },
+    {
+        number: "3",
+        content: "Step 3",
+        name: "Add-ons"
+    },
+    {
+        number: "4",
+        content: "Step 4",
+        name: "Summary"
+    }
+]
---
+++ src/MainAppForm/Main/Sidebar/styled.ts
@@ -0,0 +1,59 @@
+import styled, { css } from "styled-components";
+import bgdesktop from "./bg-sidebar-desktop.svg";
+interface ActiveStepProps {
+  activestep: "true" | "false";
+}
+
+export const StyledSidebar = styled.aside`
+  min-width: 270px;
+  border-radius: 10px;
+  background-image: url("${bgdesktop}");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
+
+export const StepsBox = styled.div`
+  padding: 20px 0;
+`;
+
+export const Step = styled.div`
+  padding: 14px 40px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  color: ${({ theme }) => theme.colors.white};
+  line-height: 1.4;
+`;
+
+export const StepNumber = styled.span<ActiveStepProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  border: 2px solid ${({ theme }) => theme.colors.white};
+  font-weight: 700;
+
+  ${({ activestep }) =>
+    activestep === "true" &&
+    css`
+      background-color: ${({ theme }) => theme.colors.lightBlue};
+      color: ${({ theme }) => theme.colors.marineBlue};
+      border: none;
+    `}
+`;
+
+export const StepContent = styled.div`
+  text-transform: uppercase;
+`;
+
+export const StepNumberName = styled.p`
+  margin: 0;
+`;
+
+export const StepName = styled.p`
+  font-weight: 700;
+  margin: 0;
+`;
