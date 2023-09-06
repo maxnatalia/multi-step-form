8c2d81f232ac9fc40f03bb908fcd23a309253da6
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-06 07:59:30 +0200

    updated interface - aligncenter and icon

--- src/MainAppForm/Main/Form/StepsLayout/interfaces.ts
+++ src/MainAppForm/Main/Form/StepsLayout/interfaces.ts
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
-
 export interface StepsLayoutProps {
+  icon?: ReactNode;
+  aligncenter?: "true" | "false";
   title: string;
   description: string;
   children: ReactNode;
