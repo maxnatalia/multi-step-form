ebb1013068f36753b81115e952f48b0d7a9b6727
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-06 07:59:07 +0200

    added props aligncenter and media queries

--- src/MainAppForm/Main/Form/StepsLayout/styled.ts
+++ src/MainAppForm/Main/Form/StepsLayout/styled.ts
@@ -1,11 +1,35 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+interface AlignCenterProps {
+  aligncenter: "true" | "false";
+}
 
-export const Container = styled.div`
+export const Container = styled.div<AlignCenterProps>`
   padding: 30px 60px;
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
   height: 90%;
   min-width: 610px;
+  background-color: ${({ theme }) => theme.colors.alabaster};
+  border-radius: 0 10px 10px 0;
+
+  ${({ aligncenter }) =>
+    aligncenter === "true" &&
+    css`
+      align-items: center;
+      justify-content: center;
+    `}
+
+  @media (max-width: 991px) {
+    min-width: 80%;
+    padding: 40px 14px;
+    margin: -80px 14px 140px 14px;
+    border-radius: 10px;
+  }
+`;
+
+export const IconWrapper = styled.div`
+  margin: 30px auto;
 `;
 
 export const Title = styled.h2`
