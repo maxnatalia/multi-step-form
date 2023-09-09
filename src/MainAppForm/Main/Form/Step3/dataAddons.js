23d9eb680b1220eb28c921fe6e74b3058aed9c7f
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-09 08:15:16 +0200

    added dataAddons for Step3

---
+++ src/MainAppForm/Main/Form/Step3/dataAddons.js
@@ -0,0 +1,23 @@
+export const dataAddons = [
+    {
+        id: 1,
+        name: "Online service",
+        description: "Access to multiplayer games",
+        priceMonthly: "+$1/mo",
+        priceYearly: "+$10/yr",
+    },
+    {
+        id: 2,
+        name: "Larger storage",
+        description: "Extra 1TB of cloud save",
+        priceMonthly: "+$2/mo",
+        priceYearly: "+$20/yr",
+    },
+    {
+        id: 3,
+        name: "Customizable Profile",
+        description: "Custom theme on your profile",
+        priceMonthly: "+$2/mo",
+        priceYearly: "+$20/yr",
+    }
+];
